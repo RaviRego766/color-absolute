c48a0f466d7e14b444fe5e73c9e6c4653585b0dc
RaviRego766 <jfrancarego@gmail.com>
2024-08-15 12:54:36 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 //% color="#ff0000" icon="\u221e"
-namespace colorabs {}
+namespace colorabs {
+
+//% block
+export function color(a : number) {}
+//% block
+export function abs.color(a : number) {
+
+
+
+
+
+}
 
